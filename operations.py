566d38c9a87d6a5cfda423cb194c620df4786bee
Alejandro Costich <113185918+alex-costich@users.noreply.github.com>
2025-08-22 20:31:39 +0000

    Initial commit

--- operations.py
+++ operations.py
@@ -1,35 +1,33 @@
-def add(a, b):
+def add(a, b): # works fine
     return a + b
 
-def subtract(a, b):
+def subtract(a, b): # works fine
     return a - b
 
-def multiply(a, b):
+def multiply(a, b): # works fine
     return a * b
 
-def divide(a, b):
-    
+def divide(a, b): # works fine
     return a / b  
 
-def power(a, b):
+def power(a, b): # doesn't work for floats or ints
     return a ^ b  
 
-def square_root(x):
+def square_root(x): # doesn't work for negatives
     import math
     return math.sqrt(x)
 
 def average(list):
-    return sum(list) / len(list)
+    return sum(list) / len(list) # works fine
 
 def maximum(list):
-    return min(list) 
+    return min(list) # min should be max
 
 
 if __name__ == "__main__":
-
     print("start test")
 
-
+    print(maximum([0, -100, 100]))
 
     print("end test")
 
